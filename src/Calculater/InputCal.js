9f56cf2bd17d320ea10f4d176e6bb240cc22dac6
phamthien123 <bangbang01213@gmail.com>
2022-08-11 16:04:02 +0700

    11/08 2

--- src/Calculater/InputCal.js
+++ src/Calculater/InputCal.js
@@ -1,7 +1,7 @@
 const InputCal = (props) =>{
     const message = props.value
     return (
-        <input type="text" size={48} value={message} disabled  className="input-text"/>
+        <input type="text" size={41} value={message} disabled  className="input-text"/>
     )
 }
 export default InputCal;
